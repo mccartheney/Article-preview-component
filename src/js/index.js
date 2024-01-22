c74ca438618dd5ed1022376d138e1677bbd01eb6
Mccartheney <mccartheney@hotmail.com>
2024-01-22 15:45:35 +0000

    fix issues from button

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,14 @@ if (window.innerWidth < 1024) {
     document.querySelector(".main_share_share_share-button").addEventListener("click", () => {
         if (popUp.style.display == "none") {
             popUp.style.display = "flex";
+            document.querySelector(".main_share_share_share-button").style.display ="none"
         }
     })
     
     document.querySelector(".main_share_popUp-button").addEventListener("click", ()  => {
         if (popUp.style.display == "flex") {
             popUp.style.display = "none";
+            document.querySelector(".main_share_share_share-button").style.display = "inline-block"
         }
     })
 }else  {
